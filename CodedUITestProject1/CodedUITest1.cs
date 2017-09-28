07a25f802c69742a314fed25e6260149dd3044ec
Alexey Konovalenko <alexey.konovalenko@comodo.com>
2017-09-28 13:09:18 +0300

    hardcode

--- CodedUITestProject1/CodedUITest1.cs
+++ CodedUITestProject1/CodedUITest1.cs
@@ -25,7 +25,8 @@ namespace CodedUITestProject1
         [TestInitialize]
         public void Init()
         {
-            testResultDir = TestContext.TestResultsDirectory;          
+            testResultDir = TestContext.TestResultsDirectory;
+            
         }
         [ClassCleanup]
         public static void GatherTestResults()
@@ -34,8 +35,8 @@ namespace CodedUITestProject1
             TRXParser parser = new TRXParser();
             //String currentDir = Directory.GetCurrentDirectory();
             Console.WriteLine(testResultDir);
-            Directory.CreateDirectory(testResultDir + "\\results");
-            string[] reportFiles = Directory.GetFiles(testResultDir, "*.trx");
+            Directory.CreateDirectory(testResultDir + "\\..\\..\\..\\results");
+            string[] reportFiles = Directory.GetFiles(testResultDir+"\\..\\..\\..\\", "*.trx");
 
             try
             {
